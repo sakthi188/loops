539e65dd7f7f0ec2bb0fbc8fd0cc7f37752f4026
sakthi188 <107858315+sakthi188@users.noreply.github.com>
2023-03-30 22:40:18 +0530

    Add files via upload

---
+++ for loop1.js.js
@@ -0,0 +1,14 @@
+var arr=[];
+for(var i=0;i<10;i+=2)
+{
+   arr.push(i);
+}
+console.log(arr);
+
+//adding odd numbers from 1 to 10
+var arr1=[];
+for(var i=1;i<=10;i+=2)
+{
+    arr1.push(i);
+}
+console.log(arr1);
